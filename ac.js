a0b73803cae07739db1a16c71f3b37e949c51062
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-22 18:07:45 +0000

    Add files via upload

--- ac.js
+++ ac.js
@@ -4,10 +4,13 @@ const targetDate = new Date("23 Dec 2020");
 var permDiff = Math.abs(new Date() - targetDate);
 const pElem = document.querySelector('p');
 setInterval(()=>{
+   pElem.style.backgroundColor="green";
+  setTimeout(()=>{ pElem.style.backgroundColor="grey";},500);
   var currentDate = new Date();
   var timeDiff = Math.abs(currentDate-targetDate);
   for(var i=0; i<spans.length;i++){
     spans[i].innerText = Math.round(timeDiff/array[i]);
-    pElem.style.width=''+Math.round((permDiff-timeDiff)/permDiff)+'%';
+    pElem.style.width=''+100*(permDiff-timeDiff/20)/permDiff+'%';
+	//console.log(((permDiff-timeDiff/20)/permDiff)*100);
   }
 },1000)
